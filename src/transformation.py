5b6ba9658d372bf81afd22f90342db6550da7c16
talmelnikov <talmelnikov430@gmai.com>
2025-07-15 04:05:27 +0300

    Add transformation logic: Age Group, Session Group, Gender encoding

--- src/transformation.py
+++ src/transformation.py
@@ -5,18 +5,17 @@ import matplotlib.pyplot as plt
 
 def transform_data(df):
     df=df.copy()
-    play_time_per_gamegener=df.groupby('GameGenre').agg({
-        'PlayTimeHours':'mean'
-    })
-    achievement_per_difficulty=df.groupby('GameDifficulty').agg({
-        'AchievementsUnlocked':'sum'
-    })
-    df['AgeGroup']=df['Age'].apply(lambda x: 'young' if x<35 else 'adult')
-    active__age=df.groupby('AgeGroup').agg({
-        'SessionsPerWeek':'sum'
-    })
+    #Categorize players into age groups (e.g., "young", "adult", "senior")
+    df['AgeGroup']=df['Age'].apply(lambda x: 'young' if x<18 else 'adult' if 18<=x<40 else 'senior')
+    #create playtime in hours per session to see more clearly who is casual, Binger or Light gamer
+    df['PlayTimePerSession']=df.apply(lambda df: df['PlayTimeHours']/(df['SessionsPerWeek']+1),axis='columns')
+    df['IsMale'] = df['Gender'].map({'male': 1, 'female': 0})
+    #transform numerical to new categorical session group column for simplify analyze
+    df['SessionGroup'] = pd.cut(
+    df['SessionsPerWeek'],         # column to cut
+    bins=[0, 5, 10, 20],           # numeric boundaries
+    labels=['low', 'medium', 'high']  # category names
+        )
+  
 
-    print("Average Play Time per Genre:\n",play_time_per_gamegener)
-    print("\nTotal Achievements per Difficulty:\n", achievement_per_difficulty)
-    print("\nSessions per Age Group:\n", active__age)
     return df
